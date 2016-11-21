51931544a1cd6a96a6fd26fc33317a20502577ee
effktor <effktor@gmail.com>
2016-11-21 03:03:42 +0100

    Initial Commit
    Initial Commit, setting up project

---
+++ Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// Class for handling input from controllers
+
+public class InputController : MonoBehaviour
+{
+    private Vector3 _lefttAxisX;
+    private Vector3 _leftAxisY;
+    private Vector3 _rightAxisX;
+    private Vector3 _rightAxisY;
+
+    /// <summary>
+    /// Returns the current left horizontal input Axis.
+    /// </summary>
+    public Vector3 LeftAxisX
+    {
+        get { return _lefttAxisX; }
+    }
+
+    /// <summary>
+    /// Returns the current left vertical input Axis.
+    /// </summary>
+    public Vector3 LeftAxisY
+    {
+        get { return _leftAxisY; }
+    }
+
+    /// <summary>
+    /// Returns the current right horizontal input Axis.
+    /// </summary>
+    public Vector3 RightAxisX
+    {
+        get { return _rightAxisX; }
+    }
+
+    /// <summary>
+    /// Returns the current right vertical input Axis.
+    /// </summary>
+    public Vector3 RightAxisY
+    {
+        get { return _rightAxisY; }
+    }
+
+    public bool IsFirePrimary
+    {
+        get;
+        private set;
+    }
+
+    public bool IsFireSecondary
+    {
+        get;
+        private set;
+    }
+
+    private void Update()
+    {
+    }
+
+    private void UpdateButtons()
+    {
+        IsFirePrimary = Input.GetButtonDown("Fire1");
+        IsFireSecondary = Input.GetButtonDown("Fire2");
+    }
+}
